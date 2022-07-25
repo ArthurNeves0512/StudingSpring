c9088657f976a6851bf35ba6f2f588cf88e7a83a
arthur <arthurcontroleambiental@gmail.com>
2022-07-24 23:08:45 -0300

    conexao feita com o bancodedados e a aplicaçao foi subida

--- parking-control/src/main/java/com/api/parkingcontrol/ParkingControlApplication.java
+++ parking-control/src/main/java/com/api/parkingcontrol/ParkingControlApplication.java
@@ -1,13 +1,21 @@
 package com.api.parkingcontrol;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ParkingControlApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ParkingControlApplication.class, args);
 	}
-
+	@GetMapping("ola")
+	public String hello(){
+		return "ola mundo";
+	}
 }
+
